b588d649ad4d97277b843e06b2bd4243ada541c2
EinarSoy <143951495+EinarSoy@users.noreply.github.com>
2024-04-24 18:47:20 +0200

    Update endre.js

--- Oblig3db/src/main/resources/static/endre.js
+++ Oblig3db/src/main/resources/static/endre.js
@@ -13,46 +13,6 @@ function hentAlleBilletter() {
         });
 }
 
-/*
-function formaterBilletter(billetter){
-    let ut = "<select id='valgtMerke' onchange='finnTyper()'>";
-    let forrigeMerke = "";
-    ut+="<option>Velg merke</option>";
-    for (const bil of biler){
-        if(bil.merke !== forrigeMerke){
-            ut+="<option>"+bil.merke+"</option>";
-        }
-        forrigeMerke = bil.merke;
-    }
-    ut+="</select>";
-    $("#merke").html(ut);
-}
-
-function finnTyper(){
-    const valgtMerke = $("#valgtMerke").val();
-    $("#feilMerke").html("");
-    $.get( "/hentBiler", function( biler ) {
-        formaterTyper(biler,valgtMerke);
-    })
-        .fail(function(jqXHR) {
-            const json = $.parseJSON(jqXHR.responseText);
-            $("#feil").html(json.message);
-        });
-}
-function formaterTyper(biler,valgtMerke){
-    let ut = "<select id='valgtType'>";
-    for(const bil of biler ){
-        if(bil.merke === valgtMerke){
-            ut+="<option>"+bil.type+"</option>";
-        }
-    }
-    ut+="</select>";
-    $("#type").html(ut);
-}
-
-
- */
-
 function henteEnBillett(){
     const id = window.location.search.substring(1); // kommer fra kallet i index.js
     const url = "/henteEnBillett?id="+id;
