fdf9cf767992132c150997866b8b9eebdd29e8d4
EinarSoy <143951495+EinarSoy@users.noreply.github.com>
2024-05-03 20:24:44 +0200

    Delete Oblig3db directory

--- Oblig3db/src/main/resources/static/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO Billetter (id, film, antall, fornavn, etternavn, telefon, epost)
-    Values ("1", 'Michael Clayton (2007)', "2", "Lillebjørn", "Nilsen", "90012003", "lillebjorn@nilsen.no")
